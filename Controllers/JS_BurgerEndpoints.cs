06ffb21b1751ef3b772de7505ba599f5ef40c053
Juliana Sosa <juliana.sosa@udla.edu.ec>
2024-05-08 11:52:31 -0500

    Agregar EndPoint de Burgers

---
+++ Controllers/JS_BurgerEndpoints.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Juliana_Sosa_API_Burger.Data;
+using Juliana_Sosa_API_Burger.Data.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+namespace Juliana_Sosa_API_Burger.Controllers;
+
+public static class JS_BurgerEndpoints
+{
+    public static void MapBurgerEndpoints (this IEndpointRouteBuilder routes)
+    {
+        var group = routes.MapGroup("/api/Burger").WithTags(nameof(Burger));
+
+        group.MapGet("/", async (JulianaSosaDbContext db) =>
+        {
+            return await db.Burgers.ToListAsync();
+        })
+        .WithName("GetAllBurgers")
+        .WithOpenApi();
+
+        group.MapGet("/{id}", async Task<Results<Ok<Burger>, NotFound>> (int burgerid, JulianaSosaDbContext db) =>
+        {
+            return await db.Burgers.AsNoTracking()
+                .FirstOrDefaultAsync(model => model.BurgerId == burgerid)
+                is Burger model
+                    ? TypedResults.Ok(model)
+                    : TypedResults.NotFound();
+        })
+        .WithName("GetBurgerById")
+        .WithOpenApi();
+
+        group.MapPut("/{id}", async Task<Results<Ok, NotFound>> (int burgerid, Burger burger, JulianaSosaDbContext db) =>
+        {
+            var affected = await db.Burgers
+                .Where(model => model.BurgerId == burgerid)
+                .ExecuteUpdateAsync(setters => setters
+                    .SetProperty(m => m.BurgerId, burger.BurgerId)
+                    .SetProperty(m => m.Nombre, burger.Nombre)
+                    .SetProperty(m => m.ConQueso, burger.ConQueso)
+                    .SetProperty(m => m.Precio, burger.Precio)
+                    );
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("UpdateBurger")
+        .WithOpenApi();
+
+        group.MapPost("/", async (Burger burger, JulianaSosaDbContext db) =>
+        {
+            db.Burgers.Add(burger);
+            await db.SaveChangesAsync();
+            return TypedResults.Created($"/api/Burger/{burger.BurgerId}",burger);
+        })
+        .WithName("CreateBurger")
+        .WithOpenApi();
+
+        group.MapDelete("/{id}", async Task<Results<Ok, NotFound>> (int burgerid, JulianaSosaDbContext db) =>
+        {
+            var affected = await db.Burgers
+                .Where(model => model.BurgerId == burgerid)
+                .ExecuteDeleteAsync();
+            return affected == 1 ? TypedResults.Ok() : TypedResults.NotFound();
+        })
+        .WithName("DeleteBurger")
+        .WithOpenApi();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Juliana_Sosa_API_Burger.Controllers;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +22,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapBurgerEndpoints();
 
 app.Run();
