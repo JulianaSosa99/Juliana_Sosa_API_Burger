b3282c7e024f07d7c5c7c71d7d9726bd3f255444
Juliana Sosa <juliana.sosa@udla.edu.ec>
2024-05-08 11:41:27 -0500

    Creacion de lacarpeta Data con la carpeta Models

---
+++ Data/Models/Burger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Juliana_Sosa_API_Burger.Data.Models;
+
+public partial class Burger
+{
+    public int BurgerId { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public bool ConQueso { get; set; }
+
+    public decimal Precio { get; set; }
+
+    public virtual ICollection<Promo> Promos { get; set; } = new List<Promo>();
+}
---
+++ Data/Models/Promo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Juliana_Sosa_API_Burger.Data.Models;
+
+public partial class Promo
+{
+    public int PromoId { get; set; }
+
+    public string? Descripcion { get; set; }
+
+    public DateTime FechaPromo { get; set; }
+
+    public int BurgerId { get; set; }
+
+    public virtual Burger Burger { get; set; } = null!;
+}
